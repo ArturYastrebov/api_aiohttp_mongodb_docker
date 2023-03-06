604489b74556a7fc50ec660dbab295b92e201b35
AOYastrebov <AOYastrebov@gmail.com>
2023-03-06 19:32:59 +0200

    added to mongo_client param aconnect=False

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 mongo_client = AsyncIOMotorClient(
     "mongodb+srv://Levs192:pas12345@cluster0.qyfuqkk.mongodb.net/DB_USERS?retryWrites=true&w=majority",
     serverSelectionTimeoutMS=10000,
+    connect=False,
 )
 mongo_db = mongo_client["DB_USERS"]
 users_collection = mongo_db["Collection_USERS"]
